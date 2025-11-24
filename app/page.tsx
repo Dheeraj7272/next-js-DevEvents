9f9bd14a03ca97be7d490b087dcc031accdb4874
Dheeraj Panchal <dheeraj@jagdushah.com>
2025-11-24 18:52:52 +0530

    Complete project

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,50 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {IEvent} from "@/database";
-import {cacheLife} from "next/cache";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
-    'use cache';
-    cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const { events } = await response.json();
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch(`${BASE_URL}/api/events`, {
+    next: { revalidate: 3600 },
+  });
 
-    return (
-        <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
-            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+  if (!response.ok) {
+    console.error("Failed to fetch events:", response.status);
+    return <section>Error loading events</section>;
+  }
 
-            <ExploreBtn />
+  const { events } = await response.json();
 
-            <div className="mt-20 space-y-7">
-                <h3>Featured Events</h3>
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
 
-                <ul className="events">
-                    {events && events.length > 0 && events.map((event: IEvent) => (
-                        <li key={event.title} className="list-none">
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    )
-}
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Page;
